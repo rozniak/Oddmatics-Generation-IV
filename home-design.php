270416583d7ad6355b51b2510da7eb677dca0831
Rory Fewell <roryfewell@hotmail.com>
2018-07-26 21:41:05 +0100

    Some initial placeholder work on designing the home page

---
+++ home-design.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Oddmatics</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Audiowide|Hammersmith+One|Nova+Square" rel="stylesheet">
+
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        <header>
+            <div id="site-logo">
+                <h1>oddmatics</h1>
+                <p>solo developments - creative productions</p>
+            </div>
+
+            <nav id="nav-main-menu">
+                <a href="#">Home</a>
+                <a href="#">Projects</a>
+                <a href="#">Concepts</a>
+                <a href="#">Blog</a>
+                <a href="#">Interactive</a>
+                <a href="#">Abouts</a>
+            </nav>
+
+            <nav id="social-items">
+                <a href="#">FPO</a>
+                <a href="#">FPO</a>
+                <a href="#">FPO</a>
+            </nav>
+
+            <div id="main-menu-hamburger">
+                <a href="#">FPO</a>
+            </div>
+        </header>
+    </body>
+</html>
